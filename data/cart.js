5f4f6737e43b0fc315735b50892007750e11ba11
parth-mishra-107 <parthmishra1121@gmail.com>
2025-06-20 16:18:05 +0530

    progressed with the chekout page

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,22 @@
-export let cart =[{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
+  quantity: 2,
+  deliveryOptionId: '1'
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+  quantity: 1,
+  deliveryOptionId: '2'
 }];
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
+
 
 export function addToCart(productId) {
   let matchingItem;
@@ -20,13 +32,15 @@ export function addToCart(productId) {
   } else {
     cart.push({
       productId: productId,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '1'
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
-  const newCart=[];
+  const newCart=[]; 
 
   cart.forEach((cartItem) => {
     if(cartItem.productId !== productId) {
@@ -35,4 +49,6 @@ export function removeFromCart(productId) {
   });
 
   cart= newCart;
+
+  saveToStorage();
 }
